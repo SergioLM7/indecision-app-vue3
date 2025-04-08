e4a8d7246b2d7a9da127717a07cc8049b450003b
Sergio Lillo <sergiolillom@gmail.com>
2025-04-08 10:47:34 +0200

    ChatBot components, interface and functionality first steps

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,9 @@
 <template>
-  <h1 class="text-3xl font-bold underline">Mi primera App</h1>
-
-  <CounterComponent :value="5" />
-  <CounterScriptComponent :value="10" />
+  <IndecisionView />
 </template>
 
 <script lang="ts" setup>
-import CounterComponent from './components/CounterComponent.vue';
-import CounterScriptComponent from './components/my-counter-script/CounterScriptComponent.vue';
+import IndecisionView from '@/views/IndecisionView.vue';
+
 
 </script>
